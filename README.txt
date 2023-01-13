8dbaa39d1a7b7a5b3ae23de9cbae0616f298ced9
kingtoni10 <marc.toni15.3@gmail.com>
2023-01-13 16:33:39 +0100

    initial upload

---
+++ README.txt
@@ -0,0 +1,7 @@
+This is a basic 3D Maze game. The maze is randomly generated. You look around for coins, when you pick up a coin, the maze changes slighly and eventually after gathering some, a multi colored cube will have to be picked up to progress to the next level
+The required dependancies for the game are GLFW, glad, glm.
+
+The source of the game is this link:
+https://www.youtube.com/watch?v=w4pB5CD2FDo
+
+I have added better comments, removed jumping, added screenshots, added some other shaders (sadly not in use, but I left them nontherless).
---
+++ build/shaders/Blue-circle.glsl
@@ -0,0 +1,22 @@
+vec3 sdfCircle(vec2 uv, float r, vec2 offset) {
+  float x = uv.x - offset.x;
+  float y = uv.y - offset.y;
+  
+  float d = length(vec2(x, y)) - r;
+  
+  return d > 0. ? vec3(1.) : vec3(0., 0.6, 1.);
+}
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord )
+{
+  vec2 uv = fragCoord/iResolution.xy; // <0,1>
+  uv -= 0.5;
+  uv.x *= iResolution.x/iResolution.y; // fix aspect ratio
+  
+  vec2 offset = vec2(0.2, 0.2); // move the circle 0.2 units to the right and 0.2 units up
+  
+  vec3 col = sdfCircle(uv, .2, offset);
+
+  // Output to screen
+  fragColor = vec4(col,1.0);
+}
